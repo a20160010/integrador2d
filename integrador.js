151a4a17db6ca2a98e1ea15fd736cdae252bd219
a20160010 <131192923+a20160010@users.noreply.github.com>
2023-09-05 12:33:43 -0500

    Add files via upload

---
+++ integrador.js
@@ -0,0 +1,15 @@
+alert("Bienvenidos al Restaurant Capitalis, esta es su cuenta");
+var entrada=parseInt(prompt("Ingrese el monto de la entrada"));
+console.log("la entrada cuesta:" ,entrada);
+var segundo=parseInt(prompt("Ingrese el monto de la entrada"));
+console.log("el segundo cuesta:" ,segundo);
+var postre=parseInt(prompt("Ingrese el monto del postre"));
+console.log("el postre cuesta:" ,postre);
+alert=presione F12 para ver el coste total
+var total=entrada+segundo+postre;
+var IGV=(total*18/100)+total; 
+console.log("la entrada cuesta:" ,entrada);
+console.log("el segundo cuesta" ,segundo)
+console.log("el postre cuesta" ,postre)
+console.log("El costo total es:" ,total);
+console.log("el total con igv es" ,IGV)
